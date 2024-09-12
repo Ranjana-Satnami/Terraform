445519a73eec7026a4e67730fb1d002452075567
Ranjana <168744892+Ranjana-Satnami@users.noreply.github.com>
2024-09-12 23:32:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 
 resource "aws_instance" "abhishek" {
   instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+  ami = "ami-053b0d53c279acc90" 
+  subnet_id = "subnet-019ea91ed9b5252e7" 
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "rancho-s3" 
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
